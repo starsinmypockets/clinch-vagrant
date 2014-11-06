5ac041d1b0feb53694a3d90a238bf7e752797e01
Paul Walker <starsinmypockets@gmail.com>
2014-11-06 08:53:57 -0500

    remove redundant mobgorestore checl

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # make sure to install vagrant and virtual box:
 command -v vagrant >/dev/null || { echo "Vagrant not found. Install vagrant at https://www.vagrantup.com/downloads.html."; exit 1; }
 command -v mongo >/dev/null || { echo "Mongo DB not found. Install mongodb to continue."; exit 1; }
-command -v mongorestore >/dev/null || { echo "mongorestore not found. Install mongorestore to continue"; exit 1; }
 if [ $# -lt 2 ]
   then
     echo "Usage: nodeDev /path/to/install/dir project_name"
