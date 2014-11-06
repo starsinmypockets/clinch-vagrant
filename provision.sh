5d69c111345ea76ca5a652b18896a1975741b6e5
Paul Walker <starsinmypockets@gmail.com>
2014-11-05 22:04:54 -0500

    add privisioning file

---
+++ provision.sh
@@ -0,0 +1,54 @@
+echo "Build clean database"
+mongorestore /home/vagrant/app/data/buildClean1 --db devLocal
+mv /home/vagrant/app/server/DEV.config.js /home/vagrant/app/server/config.js
+mkdir /home/vagrant/app/logs
+sudo npm install
+
+sudo apt-get -y install vim
+
+# put nginx in
+sudo apt-get -y install nginx
+
+echo "Writing nginx conf"
+
+sudo cat >/etc/nginx/sites-enabled/clinch <<EOF
+upstream clinch_build_server {
+  server 127.0.0.1:3003;
+}
+
+server {
+    listen 3000;
+    server_name localhost;
+    error_log  /var/log/nginx/build/nginx_error.log  warn;
+
+  location /static/ {
+    autoindex on;
+    alias /home/vagrant/app/static/;
+  }
+
+  location /server/ {
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header Host \$http_host;
+      proxy_set_header X-NginX-Proxy true;
+      proxy_pass http://clinch_build_server/;
+      proxy_redirect off;
+  }
+
+  location /sock/{
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade \$http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_pass http://127.0.0.1:3004;
+  }
+
+  location / {
+    root /home/vagrant/app/client;
+    try_files \$uri /index.html;
+  }
+}
+EOF
+
+sudo mkdir /var/log/nginx/build/
+sudo service nginx start
+sudo node /home/vagrant/app/server/router.js
